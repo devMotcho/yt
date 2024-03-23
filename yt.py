d4eb456cb3218a4fec89d7c01143871ef54e692a
devMotcho <code.mmd2@gamil.com>
2024-03-23 12:37:49 +0000

    third

--- yt.py
+++ yt.py
@@ -2,68 +2,40 @@ from pytube import Playlist, YouTube
 import os
 
 urls = []
-def download_music(item, caminho):
-    # download
-    out_file = item[0].download(caminho)
-    base, ext = os.path.splitext(out_file)
-    new_file = base + '.mp3'
-    os.rename(out_file, new_file)
-    print(yt.title + 'Downloaded!')
-
-
-# path directory download
 PATH = r"C:\Users\devmm\Desktop\Album"
-
-# Get url of download
 URL = input('>>\n')
 
-# if is a playlist
+def download_music(stream, path):
+    music = stream.streams.filter(only_audio=True)
+    music_name = stream.title.split(' ')[0]
+
+    print(f'Downloading... {music_name} - {yt.author}')
+
+    music[0].download(path)
+    print(yt.title + 'Downloaded!')
+
+
 if 'playlist' in URL:
-    # assign playlist
     playlist = Playlist(URL)
-    print(f'Downloading Playlist: {playlist.title}')
-    print('Number of Musics: %s' % len(playlist.video_urls))
-    playlist_name = playlist.title.split(' ')[0]
-
-    # get all urls from playlist
-    for url in playlist:
-        urls.append(url)
-
-    for url in urls:
-        yt = YouTube(url)
-        print(f'Starting Download... {yt.title}')
-        music = yt.streams.filter(only_audio=True)
-
-        path = f'{PATH}\\{playlist_name}'
-
-        # try create a directory with the playlist name
-        try:
-            path = os.mkdir(path)
-            download_music(music, path)
-            
-        except FileExistsError:
-            choice = input("You already downloaded this playlist, want to continue? y/n \n")
-            if choice == 'y':
-                download_music(music, path)
-            else:
-                break
+    playlist_name = playlist.title.split('-')[0]
 
     
+    try:
+        path = f'{PATH}\\{playlist_name}'
+        os.mkdir(path)
+    except FileExistsError:
+        choice = input("You already downloaded this playlist, want to continue? y/n \n")
+        if choice == 'y':
+            path = f'{PATH}\\{playlist_name}'
+        else:
+            "Ended loop"
+    
+    for url in playlist:
+            yt = YouTube(url)
+            download_music(yt,path)
 
-# if is only 1 music
+# is a music
 else:
-    #URL
-    yt = YouTube(str(URL))
-    music = yt.streams.filter(only_audio=True)
-    music_name = yt.title.split(' ')[0]
-
-    path = PATH
-
-    print(f'Downloading.... {music_name} - {yt.author}')
-    out_file = music[0].download(path)
-    base, ext = os.path.splitext(out_file)
-    new_file = f'{music_name} - {yt.author}' + '.mp3'
-    os.rename(out_file, new_file)
-    print(yt.title + 'Downloaded!')
-    print(f'{music_name} - {yt.author} has been downloaded!')
-
+    yt = YouTube(URL)
+    
+    download_music(yt, PATH)
